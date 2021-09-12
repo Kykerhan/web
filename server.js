666bf184eb305bf4e77504099074512220904516
Kykerhan <50369345+Kykerhan@users.noreply.github.com>
2021-09-12 16:30:46 +0300

    winish one part(done static folder)

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const path = require("path");
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
-})
+app.use(express.static('static'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'index.html'))
+// })
 
 app.listen(PORT, () => {
   console.log('Server has been started...')
