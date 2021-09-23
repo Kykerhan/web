17392f13f30e4925d32ff50b498db7f5617377dc
Kykerhan <50369345+Kykerhan@users.noreply.github.com>
2021-09-23 11:32:01 +0300

    done project

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require("path");
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -8,6 +9,10 @@ app.use(express.static('static'))
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'index.html'))
 // })
+
+app.get('/book', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'static', 'book.html'))
+})
 
 app.listen(PORT, () => {
   console.log('Server has been started...')
